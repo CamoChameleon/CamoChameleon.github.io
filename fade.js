4c3d30d60843f7bc8645819e6d79987035287c9b
CamoChameleon <31912400+CamoChameleon@users.noreply.github.com>
2017-09-15 11:48:31 +0800

    fade in js

--- fade.js
+++ fade.js
@@ -1,1 +1,17 @@
-asda
+$(function(){  // $(document).ready shorthand
+  $('.lux').fadeIn('slow');
+});
+  $(document).ready(function() {
+  $(window).scroll( function(){
+  $('.hideme').each( function(i){
+            
+  var bottom_of_object = $(this).position().top + $(this).outerHeight();
+  var bottom_of_window = $(window).scrollTop() + $(window).height();
+  if( bottom_of_window > bottom_of_object ){
+      $(this).animate({'opacity':'1'},1500);
+                    
+      }
+            
+    }); 
+  });
+  });
